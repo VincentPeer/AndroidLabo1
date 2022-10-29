474b3d7f12a273837df9f89f01082df12742a487
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-10-29 17:38:31 +0200

    ex 1 : check if result is null, use string resource

--- app/src/main/java/ch/heigvd/daa/labo1/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa/labo1/MainActivity.kt
@@ -8,10 +8,24 @@ import ch.heigvd.daa.labo1.contract.PickNameContract
 
 class MainActivity : AppCompatActivity() {
 
-    var name : String = ""
+    private var name : String? = null
+    private lateinit var welcomeTextView:TextView
+
+
+    private val getName = registerForActivityResult(PickNameContract()) {
+        if (it != null) {
+            name = it
+            welcomeTextView.text = getString(R.string.welcome_user, name)
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        welcomeTextView = findViewById(R.id.welcome)
+
 
         val editButton = findViewById<Button>(R.id.edit_button)
         editButton.setOnClickListener{
@@ -19,11 +33,4 @@ class MainActivity : AppCompatActivity() {
             getName.launch(name)
         }
     }
-
-    private val getName = registerForActivityResult(PickNameContract()) {
-        val fieldValue = findViewById<TextView>(R.id.welcome)
-        fieldValue.text = String.format("Bienvenue %s", it);
-        name = "$it";
-    }
-
 }
