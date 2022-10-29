941dd569bf49b41f144ffa5193c01b3d5e254b8c
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-10-29 20:29:37 +0200

    Ex 1 : fait en sorte que l'état soit restauré après avoir tourné l'écran

--- app/src/main/java/ch/heigvd/daa/labo1/MainActivity.kt
+++ app/src/main/java/ch/heigvd/daa/labo1/MainActivity.kt
@@ -1,36 +1,55 @@
 package ch.heigvd.daa.labo1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import ch.heigvd.daa.labo1.contract.PickNameContract
+
+private const val SAVED_STATE_NAME_KEY = "NAME"
 
 class MainActivity : AppCompatActivity() {
 
-    private var name : String? = null
-    private lateinit var welcomeTextView:TextView
-    private lateinit var editButton:Button
+    private var name: String? = null
+    private lateinit var welcomeTextView: TextView
+    private lateinit var editButton: Button
+
     // Collect the username through the contract with the EditName class
     private val manageUsername = registerForActivityResult(PickNameContract()) {
         if (it != null) {
             name = it
-            welcomeTextView.text = getString(R.string.welcome_user, name)
+            updateWelcomeText()
         }
 
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        name = savedInstanceState?.getString(SAVED_STATE_NAME_KEY)
+
         setContentView(R.layout.activity_main)
 
         // Retrieve button and textView
         welcomeTextView = findViewById(R.id.welcome)
         editButton = findViewById(R.id.edit_button)
 
-        editButton.setOnClickListener{
+        updateWelcomeText()
+
+        editButton.setOnClickListener {
             manageUsername.launch(name)
         }
+    }
+
+    private fun updateWelcomeText() {
+        welcomeTextView.text =
+            if (name == null) getString(R.string.ask_for_name)
+            else getString(R.string.welcome_user, name)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SAVED_STATE_NAME_KEY, name)
     }
 
 }
