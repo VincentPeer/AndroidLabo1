46ed2fe6820d0b6d5521800840cd0dd270fdc748
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-10-29 10:45:54 +0200

    add comments

--- app/src/main/java/ch/heigvd/daa/labo1/MainActivityFragment2.kt
+++ app/src/main/java/ch/heigvd/daa/labo1/MainActivityFragment2.kt
@@ -7,30 +7,33 @@ import android.widget.Button
 
 private const val ARG_COUNTER = "param_counter"
 
+// Useful doc about saving back stack state
+// https://developer.android.com/guide/fragments/saving-state
 
 class MainActivityFragment2 : AppCompatActivity() {
 
+    // Counter to attribute number to config steps
     private var counter:Int = 1
-
-    // https://developer.android.com/guide/fragments/saving-state
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_fragment2)
         if(savedInstanceState != null) {
+            // Restore activity state
             counter = savedInstanceState.getInt(ARG_COUNTER)
         } else {
+            // Create initial fragment if no saved instance available
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container_config, ConfigFragment.newInstance(counter))
                 .commit()
         }
-
 
         // Retrieve references to buttons
         val btnBack = findViewById<Button>(R.id.btn_back)
         val btnClose = findViewById<Button>(R.id.btn_close)
         val btnNext = findViewById<Button>(R.id.btn_next)
 
+        // Configure next btn action
         btnNext.setOnClickListener {
             ++counter
             supportFragmentManager.beginTransaction()
@@ -39,6 +42,7 @@ class MainActivityFragment2 : AppCompatActivity() {
                 .commit()
         }
 
+        // Configure back btn action
         btnBack.setOnClickListener {
             if (--counter == 0) {
                 closeConfig()
@@ -47,27 +51,25 @@ class MainActivityFragment2 : AppCompatActivity() {
             }
         }
 
+        // Configure close btn action
         btnClose.setOnClickListener {
             closeConfig()
         }
+
     }
 
+    // Custom saving
     override fun onSaveInstanceState(outState: Bundle) {
-        // Never called ... why ??? is it a bug ???
         super.onSaveInstanceState(outState)
         outState.putInt(ARG_COUNTER, counter)
     }
 
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-    }
-
-
+    /**
+     * Action to trigger when closing config panel
+     */
     private fun closeConfig() {
         val i = Intent(this, MainActivity::class.java)
         startActivity(i)
     }
-
 
 }
